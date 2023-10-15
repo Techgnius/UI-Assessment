78070a0824a15ef9fba0f537307d32aa5561e716
Chinedu <nedukwu@gmail.com>
2023-10-15 21:45:09 +0100

    i had to make some other adjustments

--- src/Components/TravelPackages.jsx
+++ src/Components/TravelPackages.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Indialocation from "../image/India.jpg";
 import Uklocation from "../image/France.jpg";
-import Francelocation from "../image/United Kingdom.jpg";
+import Americanlocation from "../image/U.S.A.jpg";
 import Offer1 from "../image/luxury1.jpg";
 import Offer2 from "../image/luxury2.jpg";
 import Offer3 from "../image/luxury3.jpg";
@@ -15,7 +15,7 @@ const TravelPackages = () => {
           <h1>Trending Destinations</h1>
           <div className="locationCard-container">
             <div className="locationCard">
-              <img src={Indialocation} alt="location 1" width={140} />
+              <img src={Indialocation} alt="location 1" width={170} />
               <div className="location-details">
                 <p>Bangladesh, India</p>
                 <p>Visiting the best tourist areas chosen be the audience</p>
@@ -24,7 +24,7 @@ const TravelPackages = () => {
               <i class="fa-solid fa-star">4/5</i>
             </div>
             <div className="locationCard">
-              <img src={Uklocation} alt="location 1" width={140} />
+              <img src={Uklocation} alt="location 1" width={170} />
               <div className="location-details">
                 <p>London, United Kingdom</p>
                 <p>London is one of the most beautiful places to visit</p>
@@ -33,10 +33,13 @@ const TravelPackages = () => {
               <i class="fa-solid fa-star">5/5</i>
             </div>
             <div className="locationCard">
-              <img src={Francelocation} alt="location 1" width={140} />
+              <img src={Americanlocation} alt="location 1" width={170} />
               <div className="location-details">
-                <p>Paris, France</p>
-                <p>One of our best tourist attractions</p>
+                <p>New York, U.S.A</p>
+                <p>
+                  One of our best tourist attractions. Get ready to get value
+                  for your money by experiencing unbelievable things.
+                </p>
                 <span>$440,000</span>
               </div>
               <i class="fa-solid fa-star">4/5</i>
